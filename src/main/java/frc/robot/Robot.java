ead992e5a4fb78506a9b4b46a4d78af9b0eff161
Eddy Gobel <Eddy.Gobel@sisepuedefoundation.org>
2024-07-20 14:31:56 -0700

    delete unsed things

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -115,7 +115,6 @@ public class Robot extends LoggedRobot {
     if (autonomousCommand != null) {
       autonomousCommand.cancel();
     }
-    robotContainer.setAllSetpointsZero();
   }
 
   /** This function is called periodically when disabled. */
@@ -125,7 +124,6 @@ public class Robot extends LoggedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    autonomousCommand = robotContainer.getAutonomousCommand();
 
     robotContainer.mechanismsCoastOnDisable(false);
 
@@ -149,8 +147,6 @@ public class Robot extends LoggedRobot {
     if (autonomousCommand != null) {
       autonomousCommand.cancel();
     }
-
-    robotContainer.setAllSetpointsZero();
     robotContainer.mechanismsCoastOnDisable(false);
   }
 
