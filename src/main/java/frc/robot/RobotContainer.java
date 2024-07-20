0625997c91c10772ce00d74de5a75ea9e1183665
Eddy Gobel <Eddy.Gobel@sisepuedefoundation.org>
2024-07-20 14:29:51 -0700

    create a robot container

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,116 @@
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.button.*;
+import frc.robot.Constants.*;
+import frc.robot.Subsystems.drive.*;
+import frc.robot.Subsystems.gyro.*;
+import frc.robot.Utils.*;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // Drivetrain
+  private final Gyro m_gyroSubsystem;
+  private final Drive m_driveSubsystem;
+
+  // Controllers
+  private final CommandXboxController driverController =
+      new CommandXboxController(OperatorConstants.DRIVE_CONTROLLER);
+
+  /** The container for the robot. Contains subsystems, IO devices, and commands. */
+  public RobotContainer() {
+    switch (RobotStateConstants.getMode()) {
+      case REAL:
+        // Real robot, instantiates hardware IO implementations
+        m_gyroSubsystem = new Gyro(new GyroIONavX());
+        m_driveSubsystem =
+            new Drive(
+                new ModuleIOSparkMax(0),
+                new ModuleIOSparkMax(1),
+                new ModuleIOSparkMax(2),
+                new ModuleIOSparkMax(3),
+                m_gyroSubsystem);
+        break;
+
+      case SIM:
+        // Sim robot, instantiates physics sim IO implementations
+        m_gyroSubsystem = new Gyro(new GyroIO() {});
+        m_driveSubsystem =
+            new Drive(
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                m_gyroSubsystem);
+        break;
+
+      default:
+        // Replayed robot, disables IO implementations
+        m_gyroSubsystem = new Gyro(new GyroIO() {});
+        m_driveSubsystem =
+            new Drive(
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                m_gyroSubsystem);
+        break;
+    }
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // The front of the robot is the side where the intakes are located
+    // A default command always runs unless another command is called
+
+    /** Driver Controls */
+
+    // Driving the robot
+    m_driveSubsystem.setDefaultCommand(
+        new RunCommand(
+            () ->
+                m_driveSubsystem.driveWithDeadband(
+                    driverController.getLeftX(), // Forward/backward
+                    -driverController
+                        .getLeftY(), // Left/Right (multiply by -1 bc controller axis is inverted)
+                    driverController.getRightX()), // Rotate chassis left/right
+            m_driveSubsystem));
+
+    // Resets robot heading to be wherever the front of the robot is facing
+    driverController
+        .a()
+        .onTrue(new InstantCommand(() -> m_driveSubsystem.updateHeading(), m_driveSubsystem));
+  }
+
+  /** Either Coast or Brake mechanisms depending on Disable or Enable */
+  public void mechanismsCoastOnDisable(boolean isDisabled) {
+    m_driveSubsystem.coastOnDisable(isDisabled);
+  }
+}
